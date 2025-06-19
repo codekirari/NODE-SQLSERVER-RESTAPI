e195264f80f63d1327cde62606cf71a8315fd407
codekirari <samarycardozo@gmail.com>
2025-06-19 09:02:21 -0500

    Refactor cita routes by removing unnecessary line breaks for improved readability

--- database/clinicatest.sql
+++ database/clinicatest.sql
@@ -1,1 +1,14 @@
-CREATE DATABASE clinicatest
+-- CREATE DATABASE clinicatest
+
+
+--   // en sqlserver no sirve AUTO_INCREMENT
+
+
+CREATE TABLE Paciente (
+    id_paciente INT PRIMARY KEY IDENTITY(1,1), 
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    telefono VARCHAR(15),
+    email VARCHAR(100)
+);
--- src/routes/cita.routes.js
+++ src/routes/cita.routes.js
@@ -11,13 +11,9 @@ import {
 const router = Router();
 
 router.get("/citas", getCitas);
-
 router.get("/citas/:id", getCita);      
-
 router.post("/citas", createCita);
-
 router.put("/citas/:id", updateCita);   
-
 router.delete("/citas/:id", deleteCita);
 
 export default router;
